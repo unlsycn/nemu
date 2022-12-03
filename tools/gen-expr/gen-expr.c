81d64b82e5c964e23d779badae761f06891aa4c0
unlsycn <unlsycn@unlsycn.com>
2022-12-04 03:55:51 +0800

    test(parser): add tests to the expression evaluation

--- tools/gen-expr/gen-expr.c
+++ tools/gen-expr/gen-expr.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,32 +8,83 @@
 
 // this should be enough
 static char buf[65536] = {};
+static int loc = 0;
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format = "#include <stdio.h>\n"
+                           "#include <stdint.h>\n"
                            "int main() { "
-                           "  unsigned result = %s; "
-                           "  printf(\"%%u\", result); "
+                           "  uint64_t result = %s; "
+                           "  printf(\"%%lu\\n\", result); "
                            "  return 0; "
                            "}";
 
-static void gen_rand_expr()
+#define gen_spaces(n)               \
+    for (int i = 0; i < r % n; i++) \
+    {                               \
+        buf[loc] = ' ';             \
+        loc++;                      \
+    }
+
+#define gen_num(num)                 \
+    sprintf(&buf[loc], "%dul", num); \
+    loc += (int)(log10(num) + 3);
+
+#define RANGE 1000;
+
+static const char *op[] = {"+", "-", "*", "/", "%"};
+static const int op_prob[] = {0, 400, 440, 600, 970, 1000};
+
+static void gen_rand_expr(int n)
 {
-    buf[0] = '\0';
+    int r = rand() % RANGE;
+    gen_num(r + 1); // log10(0) equals -inf
+    while (n > 0)
+    {
+        r = rand() % RANGE;
+        for (int i = 0; i < 15; i++)
+        {
+            if (r >= op_prob[i] && r <= op_prob[i + 1])
+            {
+                sprintf(&buf[loc], "%s", op[i]);
+                loc += strlen(op[i]);
+                break;
+            }
+        }
+        gen_spaces(4);
+        if (r % 20)
+        {
+            gen_num(r + 1);
+        }
+        else
+        {
+            buf[loc++] = '(';
+            int dn = n >= 4 ? r % (n / 4) + 1 : 1;
+            gen_rand_expr(dn);
+            n -= dn;
+            buf[loc++] = ')';
+        }
+        gen_spaces(5);
+        n--;
+    }
+    return;
 }
 
 int main(int argc, char *argv[])
 {
     int seed = time(0);
     srand(seed);
-    int loop = 1;
+    int loop = LOOP;
     if (argc > 1)
     {
         sscanf(argv[1], "%d", &loop);
     }
-    int i;
-    for (i = 0; i < loop; i++)
+    int i, ret;
+    system("rm /tmp/.result /tmp/.input");
+    for (i = 0; i < loop;)
     {
-        gen_rand_expr();
+        loc = 0;
+        memset(buf, 0, sizeof(buf));
+        gen_rand_expr(DEPTH);
 
         sprintf(code_buf, code_format, buf);
 
@@ -40,19 +92,22 @@ int main(int argc, char *argv[])
         assert(fp != NULL);
         fputs(code_buf, fp);
         fclose(fp);
-
-        int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+        ret = system("clang -Werror /tmp/.code.c -o /tmp/.expr");
         if (ret != 0)
             continue;
 
-        fp = popen("/tmp/.expr", "r");
+        ret = system("/tmp/.expr >> /tmp/.result");
+        assert(ret == 0);
+        fp = fopen("/tmp/.input", "a+");
         assert(fp != NULL);
-
-        int result;
-        fscanf(fp, "%d", &result);
-        pclose(fp);
-
-        printf("%u %s\n", result, buf);
+        fputs(buf, fp);
+        fputs("\n", fp);
+        fclose(fp);
+        i++;
     }
+    ret = system("sed -i 's/^/p /' /tmp/.input");
+    ret += system("sed -i 's/ul//g' /tmp/.input");
+    ret += system("sed -i '$a q' /tmp/.input");
+    assert(ret == 0);
     return 0;
 }
