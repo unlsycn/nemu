0d1cf115e9c2d0c942d50b353404c33a670f3f14
unlsycn <unlsycn@unlsycn.com>
2023-03-29 21:33:44 +0800

    feat: implement difftest

--- src/isa/riscv64/difftest/dut.c
+++ src/isa/riscv64/difftest/dut.c
@@ -2,9 +2,19 @@
 #include <cpu/difftest.h>
 #include <isa.h>
 
+extern const char *regs[];
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc)
 {
-    return false;
+    ref_difftest_regcpy(ref_r, DIFFTEST_TO_DUT);
+    if (!difftest_check_reg("pc", pc, ref_r->pc, cpu.pc))
+        return false;
+    for (int i = 0; i < 32; i++)
+    {
+        if (!difftest_check_reg(regs[i], pc, ref_r->gpr[i], gpr(i)))
+            return false;
+    }
+    return true;
 }
 
 void isa_difftest_attach()
