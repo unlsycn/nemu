ef2e2019d99efad6f6a10beb4273f49e2cb6ebdb
unlsycn <unlsycn@unlsycn.com>
2023-11-06 00:26:53 +0800

    feat(inst): remove unused insts and add illegal inst exception

--- scripts/native.mk
+++ scripts/native.mk
@@ -10,7 +10,7 @@ $(BINARY): compile_git
 
 # Some convenient rules
 
-override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
+override ARGS += --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 export LSAN_OPTIONS := suppressions=$(NEMU_HOME)/leaks.sup
 
