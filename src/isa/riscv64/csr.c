297d77753581db92428acc280a2c9d19e7a91bd7
unlsycn <unlsycn@unlsycn.com>
2024-02-03 15:21:07 +0800

    feat(csr): implement more CSRs

--- src/isa/riscv64/csr.c
+++ src/isa/riscv64/csr.c
@@ -2,49 +2,74 @@
 
 #include <isa.h>
 
-word_t csr_space[4096];
-bool csr_valid[4096];
+#include "common.h"
+
+#define CSR cpu.csr
+csr_t csr_space[4096];
 
 #define _CSR_NAME(name, addr) STR(name),
+#define _CSR_ADDR(name, addr) addr,
 const char *csr_name[] = {MAP(CSRS, _CSR_NAME)};
+const uint16_t csr_addr[] = {MAP(CSRS, _CSR_ADDR)};
+const size_t csr_count = sizeof(csr_name) / sizeof(char *);
+
+#define SSTATUS_MASK 0x80000003000De762
+void sync_sstatus()
+{
+    CSR.sstatus->val = CSR.mstatus->val & SSTATUS_MASK;
+}
+
+void sync_mstatus()
+{
+    CSR.mstatus->val = (CSR.mstatus->val & (~SSTATUS_MASK)) | CSR.sstatus->val;
+}
 
 void init_csr()
 {
-#define CSR_INIT(name, addr)                               \
-    cpu.csr.name = (CONCAT(name, _t *)) & csr_space[addr]; \
-    csr_valid[addr] = true;
+#define CSR_INIT(name, _addr)                           \
+    CSR.name = (CONCAT(name, _t *)) & csr_space[_addr]; \
+    CSR.name->valid = true;                             \
+    CSR.name->addr = _addr;
 
     MAP(CSRS, CSR_INIT);
-    cpu.csr.mstatus->val = 0xa00001800;
-}
-
-inline bool check_csr_valid(uint16_t addr)
-{
-    return csr_valid[addr];
+    CSR.mstatus->val = 0xa00001800;
+    CSR.sstatus->val = 0x200000000;
 }
 
 word_t csr_read(uint16_t addr)
 {
-    if (check_csr_valid(addr))
-        return csr_space[addr];
+    csr_t *csr = &csr_space[addr];
+    if (csr->valid)
+        return csr->val;
     else
         panic("TODO: CSR at %x invalid to read. Should raise a instruction exception.", addr);
 }
 
 void csr_write(uint16_t addr, word_t data)
 {
-    if (check_csr_valid(addr))
-        csr_space[addr] = data;
+    csr_t *csr = &csr_space[addr];
+    if (csr->valid)
+        csr->val = data;
     else
         panic("TODO: CSR at %x invalid to write. Should raise a instruction exception.", addr);
+
+    // sync mstatus and sstatus
+    if (csr->addr == CSR.mstatus->addr)
+        sync_sstatus();
+    else if (csr->addr == CSR.sstatus->addr)
+    {
+        csr->val &= SSTATUS_MASK; // ignore illegal value
+        sync_mstatus();
+    }
 }
 
 void csr_display(CPU_state *state)
 {
     word_t **csr = (word_t **)&state->csr;
-    for (int i = 0; i < sizeof(CSRs) / sizeof(mepc_t *); i++)
+    for (int i = 0; i < sizeof(CSRs) / sizeof(csr_t *); i++)
     {
-        printf(ANSI_FMT("%-8s", ANSI_FG_MAGENTA) FMT_WORD_LH "  " FMT_WORD_LD "\n", csr_name[i], **csr, **csr);
+        if (*csr)
+            printf(ANSI_FMT("%-8s", ANSI_FG_MAGENTA) FMT_WORD_LH "  " FMT_WORD_LD "\n", csr_name[i], **csr, **csr);
         csr++;
     }
 }
--- src/isa/riscv64/include/csr.h
+++ src/isa/riscv64/include/csr.h
@@ -4,26 +4,48 @@
 #include <common.h>
 
 #define MXLEN 64
-#define CSRS(f) f(mepc, 0x341) f(mtvec, 0x305) f(mstatus, 0x300) f(mcause, 0x342) f(mcycle, 0xc00) f(satp, 0x180)
+#define SXLEN 64
+#define CSRS(f)                                                                                                          \
+    f(mepc, 0x341) f(mtvec, 0x305) f(mstatus, 0x300) f(mcause, 0x342) f(mcycle, 0xc00) f(mscratch, 0x340) f(satp, 0x180) \
+        f(sepc, 0x141) f(stvec, 0x105) f(sstatus, 0x100) f(scause, 0x142) f(sscratch, 0x140) f(medeleg, 0x302)           \
+            f(mideleg, 0x303)
 
 #define _WORD_T(val) word_t val;
-#define CSR_TYPE(name, ...)           \
-    typedef union {                   \
-        struct                        \
-        {                             \
-            MAP(_WORD_T, __VA_ARGS__) \
-        };                            \
-        word_t val;                   \
+#define CSR_TYPE(name, ...)               \
+    typedef struct                        \
+    {                                     \
+        union {                           \
+            struct                        \
+            {                             \
+                MAP(_WORD_T, __VA_ARGS__) \
+            };                            \
+            word_t val;                   \
+        };                                \
+        bool valid;                       \
+        uint16_t addr;                    \
     } CONCAT(name, _t);
 
-CSR_TYPE(mepc, mepc : MXLEN)
-CSR_TYPE(mtvec, mode : 2, base : MXLEN - 2)
+// CSR type template
+CSR_TYPE(csr, csr : 64);
+
+CSR_TYPE(mepc, mepc : MXLEN);
+CSR_TYPE(mtvec, mode : 2, base : MXLEN - 2);
 CSR_TYPE(mstatus, WPRI1 : 1, SIE : 1, WPRI2 : 1, MIE : 1, WPRI3 : 1, SPIE : 1, UBE : 1, MPIE : 1, SPP : 1, VS : 2, MPP : 2,
          FS : 2, XS : 2, MPRV : 1, SUM : 1, MXR : 1, TVM : 1, TW : 1, TSR : 1, WPRI4 : 9, UXL : 2, SXL : 2, SBE : 1, MBE : 1,
-         WPRI5 : 25, SD : 1)
-CSR_TYPE(mcause, code : MXLEN - 1, intr : 1)
-CSR_TYPE(mcycle, cycle : MXLEN)
-CSR_TYPE(satp, ppn : 44, asid : 16, mode : 4)
+         WPRI5 : 25, SD : 1);
+CSR_TYPE(mcause, code : MXLEN - 1, intr : 1);
+CSR_TYPE(mcycle, cycle : MXLEN);
+CSR_TYPE(mscratch, mscratch : MXLEN);
+CSR_TYPE(medeleg, no : SXLEN);
+CSR_TYPE(mideleg, no : SXLEN);
+
+CSR_TYPE(satp, ppn : 44, asid : 16, mode : 4);
+CSR_TYPE(sepc, sepc : SXLEN);
+CSR_TYPE(stvec, mode : 2, base : SXLEN - 2);
+CSR_TYPE(sstatus, WPRI1 : 1, SIE : 1, WPRI2 : 3, SPIE : 1, UBE : 1, WPRI3 : 1, SPP : 1, VS : 2, WPRI4 : 2, FS : 2, XS : 2,
+         WPRI5 : 1, SUM : 1, MXR : 1, WPRI6 : 12, UXL : 2, WPRI7 : 29, SD : 1);
+CSR_TYPE(scause, code : SXLEN - 1, intr : 1);
+CSR_TYPE(sscratch, sscratch : SXLEN);
 #undef _WORD_T
 
 typedef struct
